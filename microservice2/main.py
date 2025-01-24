1edd8e155e21253865c75807282bff8ce585d670
Oni Emmanuel <44571157+emmaxyz90@users.noreply.github.com>
2025-01-24 03:45:16 +0100

    Update main.py

--- microservice2/main.py
+++ microservice2/main.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-PROCESSED_ORDERS = []
+PROCESSED_ORDERS = []  # Simulates a database in memory
 
 @app.post("/process-order")
 def process_order(order: dict):
-    PROCESSED_ORDERS.append(order)
+    """
+    Endpoint to process an order received from Microservice 1.
+    """
+    PROCESSED_ORDERS.append(order)  # Save to in-memory list
     return {"message": "Order processed successfully", "order": order}
